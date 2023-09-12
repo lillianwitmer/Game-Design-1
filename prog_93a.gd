cf655ef66f61543ffb2d0045cce8fb413b8771aa
lillianwitmer <144833697+lillianwitmer@users.noreply.github.com>
2023-09-12 12:50:41 -0500

    update

--- prog_93a.gd
+++ prog_93a.gd
@@ -9,3 +9,14 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_button_3_pressed():
+	get_tree().exit()
+
+
+func _on_btncalc_pressed():
+	var watts = str($txtwatts.text)
+	var rate = watts * .0475
+	var surc = rate * .10
+	var 

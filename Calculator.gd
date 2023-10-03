9100d67934627f126c0a8631eed7c37ad46a6551
lillianwitmer <144833697+lillianwitmer@users.noreply.github.com>
2023-10-03 12:49:16 -0500

    lol

--- Calculator.gd
+++ Calculator.gd
@@ -1,7 +1,20 @@
 extends Control
+var num1 = int($num1.text)
+var num2 = int($num2.text)
 
+func add():
+	num1 + num2
+func min():
+	num1-num2
+func mul():
+	num1*num2
+func div():
+	num1/num2
+func pow():
+	num1**num2
+func MOD():
+	
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 

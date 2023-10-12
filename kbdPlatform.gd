4a3badc16f6db9e45d969de311338603e0a4cfbe
lillianwitmer <144833697+lillianwitmer@users.noreply.github.com>
2023-10-12 12:49:12 -0500

    lol

---
+++ kbdPlatform.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+
+
+func _ready():
+	$world/CollisionPolygon2D/Polygon2D.polygon = $world/CollisionPolygon2D.polygon
+
+
+
+func _process(delta):
+	pass
--- kbrdenemy.gd
+++ kbrdenemy.gd
@@ -16,6 +16,6 @@ func _on_timer_timeout():
 
 
 func _on_body_entered(body):
-	if body.name == "kdbplayer":
+	if body.name == "kbrdplayer":
 		body.queue_free() #delete player
 		OS.alert("YOURE DEAD, sucks.")

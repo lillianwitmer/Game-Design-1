cdfc17ca5b41a780a768122060724599b873ef41
lillianwitmer <144833697+lillianwitmer@users.noreply.github.com>
2023-11-10 12:51:12 -0600

    hgjjhgjgjg

--- TDplayer.gd
+++ TDplayer.gd
@@ -1,25 +1,25 @@
 extends CharacterBody2D
 
 
-const SPEED = 100.0
-#const JUMP_VELOCITY = -400.0
-const MAX_HEALTH = 400.0
+const SPEED = 100.0  # 80.0
+# const JUMP_VELOCITY = -400.0
+const MAX_OBTAINABLE_HEALTH = 400.0
 
 @export var data = {
-	"max_health": 60.0, # 20hp per heart; 5 per fraction
-	"health": 60.0, # min 60 max 400
+	"max_health": 60.0,  # 20hp per heart; 5 per fraction
+	"health": 60.0,      # Min 60 Max 400
 }
 
 var inertia = Vector2()
-var look_direction = Vector2.DOWN #(0, 1)
+var look_direction = Vector2.DOWN  # (0, 1)
 
 var menu_scene = preload("res://my_gui.tscn")
-var menu_instance = null 
+var menu_instance = null
 
-@onready var P_HUD = get_tree().get_first_node_in_group("HUD")
+@onready var p_HUD = get_tree().get_first_node_in_group("HUD")
 
 func _ready():
-	P_HUD.show()
+	p_HUD.show()
 	menu_instance = menu_scene.instantiate()
 	get_tree().get_root().add_child.call_deferred(menu_instance)
 	menu_instance.hide()
@@ -28,14 +28,14 @@ func _physics_process(delta):
 	var direction = Vector2(
 		Input.get_axis("ui_left", "ui_right"),
 		Input.get_axis("ui_up", "ui_down")
-	).normalized()
+	).normalized()  # Scale to 1 to prevent speed boost
 	update_animation(direction)
 	if direction.length() > 0:
-		look_direction = direction 
+		look_direction = direction
 		velocity = direction * SPEED
 	else:
-		velocity = velocity.move_toward(Vector2(), SPEED) 
-		
+		velocity = velocity.move_toward(Vector2(), SPEED)
+	
 	velocity += inertia
 	move_and_slide()
 	inertia = inertia.move_toward(Vector2(), delta * 1000.0)
@@ -43,28 +43,35 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("ui_cancel"):
 		menu_instance.show()
 		get_tree().paused = true
-		
+
+
 func update_animation(direction):
-	var a_name = "default"
+	var a_name = "idle_down"  # Default
 	if direction.length() > 0:
 		look_direction = direction
-		a_name = "walk"
+		a_name = "walk_"
 		if direction.x != 0:
-			a_name+="side"
+			a_name += "side"
 			$AnimatedSprite2D.flip_h = direction.x < 0
-		elif direction.y < 0 : 
+		elif direction.y < 0:
 			a_name += "up"
-		elif direction.y > 0 :
-			a_name+="walk"
+		elif direction.y > 0:
+			a_name += "down"
 		$AnimatedSprite2D.play()
-	else : 
-		if look_direction.x != 0 :
-			a_name = "default side"
-			$AnimatedSprite2D.flip_h= look_direction.x < 0 
-		elif look_direction.y < 0 :
-			a_name = "default up"
+	else:
+		if look_direction.x != 0:
+			a_name = "idle_side"
+			$AnimatedSprite2D.flip_h = look_direction.x < 0
+		elif look_direction.y < 0:
+			a_name = "idle_up"
 		elif look_direction.y > 0:
-			a_name = "default"
-		
+			a_name = "idle_down"
+	
 	if $AnimatedSprite2D.animation != a_name:
 		$AnimatedSprite2D.animation = a_name
+
+
+
+
+
+
--- playerHUD.gd
+++ playerHUD.gd
@@ -1,7 +1,7 @@
 extends CanvasLayer
 
 @onready var player = get_tree().get_first_node_in_group("player")
-@onready var hearts = $PlayerHealth/hearts
+@onready var hearts = $PlayerHealth/Hearts
 
 const HEART_ROW_SIZE = 10
 const HEART_OFFSET = 16 
@@ -37,4 +37,12 @@ func _process(delta):
 		var x = (index % HEART_ROW_SIZE) * HEART_OFFSET
 		var y = (index / HEART_ROW_SIZE) * HEART_OFFSET
 		heart.position = Vector2 (x,y)
+		
+		# frame 8 empty, 7 1/4, 6 half, 5 3/4, 4 full
+		if index > full_hearts:
+			heart.frame = 8
+		elif index == full_hearts: 
+			heart.frame = 8 - int(remainder / 5 )
+		elif index < full_hearts:
+			heart.frame = 4
 	pass

cd3e45e4244feb214b40e37a28555dc7a1a14e9c
lillianwitmer <144833697+lillianwitmer@users.noreply.github.com>
2023-11-14 12:52:19 -0600

    grgdrtrsytrs6ur

--- TDplayer (1).gd
+++ TDplayer (1).gd
@@ -8,7 +8,7 @@ const MAX_OBTAINABLE_HEALTH = 400.0
 @export var data = {
 	"max_health": 60.0,  # 20hp per heart; 5 per fraction
 	"health": 60.0,      # Min 60 Max 400
-	"money": 0 
+	"money": 0,
 }
 
 var inertia = Vector2()
@@ -18,9 +18,6 @@ var menu_scene = preload("res://my_gui.tscn")
 var menu_instance = null
 
 @onready var p_HUD = get_tree().get_first_node_in_group("HUD")
-
-func pickup_money(value):
-	data.money += value
 
 func _ready():
 	p_HUD.show()
@@ -53,7 +50,7 @@ func update_animation(direction):
 	var a_name = "idle_down"  # Default
 	if direction.length() > 0:
 		look_direction = direction
-		a_name = "walk_side"
+		a_name = "walk_"
 		if direction.x != 0:
 			a_name += "side"
 			$AnimatedSprite2D.flip_h = direction.x < 0
@@ -73,8 +70,6 @@ func update_animation(direction):
 	
 	if $AnimatedSprite2D.animation != a_name:
 		$AnimatedSprite2D.animation = a_name
-
-
 
 
 
--- changelvl.gd
+++ changelvl.gd
@@ -9,4 +9,4 @@ extends Area2D
 func _on_body_entered(body):
 	if body.is_in_group("player"):
 		var lvl = "res://" + next_level + ".tscn"
-		get_tree().change_scene
+		get_tree().change_scene_to_file(lvl)

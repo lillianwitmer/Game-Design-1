45739d868253ba40ae0e2b52b657fc07a89e8a55
lillianwitmer <144833697+lillianwitmer@users.noreply.github.com>
2023-11-29 12:59:52 -0600

    polpojkaljdtuhykjgslkrdg

--- TDenemy.gd
+++ TDenemy.gd
@@ -1,13 +1,12 @@
 extends CharacterBody2D
 
-
-const SPEED = 300.0
+const SPEED = 60.0
 var MAX_HEALTH: float = 30.0
 var HEALTH = MAX_HEALTH
 var DAMAGE = 10.0
 var AI_STATE = STATES.IDLE
 
-enum STATES { IDLE=0, DAMAGED, UP, DOWN, LEFT, RIGHT, UP_L, UP_R, DOWN_L, DOWN_R }
+enum STATES { IDLE=0, UP, DOWN, LEFT, RIGHT, UP_L, UP_R, DOWN_L, DOWN_R, DAMAGED }
 
 var state_direction = [
 	Vector2.ZERO,
@@ -21,3 +20,60 @@ var state_direction = [
 	Vector2(1, 1).normalized(),
 	Vector2.ZERO
 ]
+
+var inertia = Vector2()
+var ai_timer_max = 0.5 
+var ai_timer = ai_timer_max - randi() % 5
+var damage_lock = 0.0 
+var animation_lock = 0.0 
+var knockback = 128.0
+var vision_distance = 40.0
+var money_value = 5.0
+
+signal recovered 
+@onready var raycastR = $RayCast2DR
+@onready var raycastL = $RayCast2DL
+@onready var raycastM = $RayCast2DM
+
+func turn_toward_player_location(location: Vector2):
+	# set the state to  move towards player 
+	var dir_to_player = (location - global_position).normalized()
+	velocity = dir_to_player * (SPEED * 2)
+	# determine closest cardinal direction for animation 
+	var closest_angle = INF
+	var closest_state = STATES.IDLE
+	for i in range(1, 5):
+		var state_dir = state_direction[i]
+		var angle_diff = abs(state_dir.angle_to(dir_to_player))
+		if angle_diff < closest_angle:
+			closest_angle = angle_diff
+			closest_state = STATES.values()[i]
+	AI_STATE = closest_state
+
+func _physics_process(delta):
+	animation_lock = max(animation_lock - delta, 0.0)
+	damage_lock = max(damage_lock - delta, 0.0)
+	if int(AI_STATE) >= 1 and int(AI_STATE) <= 0:
+		var raydir = state_direction[int(AI_STATE)]
+		raycastM.target_position = raydir * vision_distance
+		raycastL.target_position = raydir.rotated(deg_to_rad(-45)).normalized() * vision_distance 
+		raycastR.target_position=  raydir.rotated(deg_to_rad(45)).normalized() * vision_distance 
+	
+	if animation_lock == 0.0:
+		if AI_STATE == STATES.DAMAGED:
+			AI_STATE = STATES.IDLE
+			recovered.emit()
+		
+		for player in get_tree().get_nodes_in_group("player"):
+			if $AttackBox.overlaps_body(player):
+				if player.damage_lock == 0.0:
+					player.inertia = (player.global_position - global_position).normalized()* knockback
+					player.take_damage(DAMAGE)
+				else:
+					return
+			if player.data.state != player.STATE.DEAD:
+				if (raycastM.is_colliding() and raycastM.get_collider() == player) or \
+				(raycastL.is_colliding() and raycastL.get_collider() == player) or \
+				(raycastR.is_colliding() and raycastR.get_collider() == player) :
+					turn_toward_player_location(player.global_position)
+	pass
